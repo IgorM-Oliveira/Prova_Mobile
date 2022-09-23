07bb9dca73feab73c48da52fc4b35e6514e7541a
Laboratorio <laboratorio@lab.unigran>
2022-09-23 19:24:16 -0400

    version 0.0.2 - entities

--- app/src/main/java/br/unigran/prova/entidades/Consumo.java
+++ app/src/main/java/br/unigran/prova/entidades/Consumo.java
@@ -1,2 +1,53 @@
-package br.unigran.prova.entidades;public class Consumo {
+package br.unigran.prova.entidades;
+
+public class Consumo {
+    private Integer id;
+    private String quiloAtual;
+    private String quantAbast;
+    private String dia;
+    private String valor;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getQuiloAtual() {
+        return quiloAtual;
+    }
+
+    public void setQuiloAtual(String quiloAtual) {
+        this.quiloAtual = quiloAtual;
+    }
+
+    public String getQuantAbast() {
+        return quantAbast;
+    }
+
+    public void setQuantAbast(String quantAbast) {
+        this.quantAbast = quantAbast;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String toString() {
+        return (String.format("%s | %s | %s", quiloAtual, quantAbast, dia, valor));
+    }
 }
